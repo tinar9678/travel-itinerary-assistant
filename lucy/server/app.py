9792c90b1df91e452fbd303ea481a74e61c4776d
tinar9678 <tina.r9678@gmail.com>
2023-08-09 16:15:59 -0400

    Add dotenv imports

--- lucy/server/app.py
+++ lucy/server/app.py
@@ -3,9 +3,11 @@ import openai
 import json
 from flask import Flask, request
 from flask_cors import CORS
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/", methods=("GET", "POST"))
