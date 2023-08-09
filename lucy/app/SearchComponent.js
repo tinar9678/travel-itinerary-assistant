fae4e00f80e133de321022a78ef730661c9d4a70
tinar9678 <tina.r9678@gmail.com>
2023-08-09 14:37:22 -0400

    Added search input and filter component

---
+++ lucy/app/SearchComponent.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import './SearchComponent.css'
+
+export default function SearchComponent() {
+    const [inputString, setInputString] = useState("");
+    const [filterselectedOption, setFilterSelectedOption] = useState(DURATIONS[0]);
+    
+    const handleInputChange = (value) => {
+        setInputString(value);
+    }
+    
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        // fetch from python server here.
+        const response = await fetch('', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                message: inputString + "*" + filterselectedOption,
+            })
+        });
+
+        const json = await response.json();
+        console.log(json.data); // TODO(tinaryu): Set output itinerary component
+    }
+
+    return (
+      <div className='search'>
+        <form onSubmit={(e) => onSubmit(e)}>
+            <input className='search-input' 
+                placeholder='where do you want to go?' 
+                onChange={(e) => handleInputChange(e.target.value)}>
+            </input>
+        </form>
+        <DurationFilterComponent 
+            filterselectedOption={filterselectedOption} 
+            setFilterSelectedOption={setFilterSelectedOption}>
+        </DurationFilterComponent>
+      </div>
+    )
+}
+
+function DurationFilterComponent({filterselectedOption, setFilterSelectedOption}) {
+    const onFilterChange = (value) => {
+        setFilterSelectedOption(value);
+    }
+    
+    const filterOptions = DURATIONS.map((duration, id) => {
+        const key = id+'-days';
+        return (
+            <option key={key} value={duration}>{duration}</option>
+        )
+    })
+
+    return (
+        <div className='duration-filter'>
+            <label htmlFor="filter">Duration</label>
+            <select className='filter' id='filter' value={filterselectedOption} onChange={(e) => onFilterChange(e.target.value)}>
+                {filterOptions}
+            </select>
+        </div>
+    )
+}
+
+const DURATIONS = ['1 day', '2 days', '3 days', '4 days', '5 days', '6 days'];
--- lucy/app/page.js
+++ lucy/app/page.js
@@ -1,11 +1,20 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import logo from './images/fairy-temp.png'
+import SearchComponent from './SearchComponent'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-      </div>
+      <Image
+              src={logo}
+              alt='Lucy: Your Personal Travel Agent'
+              className={styles.logo}
+              priority
+            />
+    <SearchComponent></SearchComponent>
     </main>
   )
 }
